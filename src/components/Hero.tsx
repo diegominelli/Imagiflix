70b1434f621c9595d598f0c835e9e30a4479605f
Diego Minelli <diego.minelli@hotmail.com>
2021-06-18 21:06:43 -0300

    Populando o Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
-import placeholder  from '../assets/backdrop.jpg';
+import placeholder from '../assets/backdrop.jpg';
 
-const Hero = () => (
-  <header className='min-h-screen'>
-    <img className='object-cover object-center h-full' src={placeholder} alt='Filme em destaque' />
-  </header>
-);
+const Hero = ({ title = 'Avengers Endgame', score = 10 }) => {
+  const getBorderColor = () => {
+    if (score >= 7) {
+      return 'border-green-400';
+    } else if (score > 4 && score < 7) {
+      return 'border-yellow-400';
+    } else {
+      return 'border-red-400';
+    }
+  };
+
+  return (
+    <header className="relative min-h-screen">
+      <img
+        className="object-cover object-center h-full"
+        src={placeholder}
+        alt="Filme em destaque"
+      />
+      <article className="absolute bottom-0 mb-64 px-8">
+        <p className="text-3xl">Assista agora:</p>
+        <h2 className="text-6xl font-bold">{title}</h2>
+        <p className="text-base">
+          Nota{' '}
+          <span
+            className={`inline-block mx-2 py-2 px-3 border-4 bg-black bg-opacity rounded-full ${getBorderColor()}`}
+          >
+            {score}
+          </span>
+        </p>
+        <button className="text-base">Assistir</button>
+        <button className="text-base">Minha Lista</button>
+      </article>
+    </header>
+  );
+};
 
 export default Hero;
