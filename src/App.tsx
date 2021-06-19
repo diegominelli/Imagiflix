30e4129d22fcfaac528bd63d3ae29cb2066705a3
Diego Minelli <diego.minelli@hotmail.com>
2021-06-18 22:17:41 -0300

    Adicionando o React Slick

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 const App = () => {
   return (
     <div className="m-auto antialised font-sans bg-black text-white">
@@ -10,6 +13,6 @@ const App = () => {
       <NavBar />
     </div>
   );
-}
+};
 
 export default App;
