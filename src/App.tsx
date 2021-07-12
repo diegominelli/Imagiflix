c39bac9918998f2d687c03e107a6cc70b5679b0f
Diego Minelli <diego.minelli@hotmail.com>
2021-07-11 21:51:15 -0300

    Fetch e Async Await

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
@@ -8,6 +8,23 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const App = () => {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=1e3d3529de8649deba42e604199b2017&language=pt-BR&sort_by=popularity.desc'
+      );
+
+      const data = await response.json();
+
+      setMovies(data);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => movies && console.log(movies), [movies]);
+
   return (
     <div className="m-auto antialised font-sans bg-black text-white">
       <Hero />
